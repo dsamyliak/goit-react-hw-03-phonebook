b179bd34511549c6de2ff79297d08caa5ad9485e
Dmytro Samyliak <dsamyliak@gmail.com>
2022-02-03 21:38:08 +0200

    add DidMount and DidUpdate methods to App.js. Contacts will save to localstorage and get from localstorage after page's start

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import PropTypes from "prop-types";
 class App extends React.Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
@@ -55,11 +55,31 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+
     console.log("3 DidMount done!");
+
+    const contacts = localStorage.getItem('contacts');
+    console.log("DidMount contacts = ", contacts);
+
+    const parsedContacts = JSON.parse(contacts);
+    console.log("parsedContacts = ", parsedContacts);
+
+    this.setState({ contacts: parsedContacts, });
+
   };
 
-  componentDidUpdate() { 
+  componentDidUpdate(prevProps, prevState) { 
+
     console.log("2 DidUpdate done");
+    console.log("this.state = ", this.state);
+    console.log("prevState = ", prevState);
+
+    if (this.state.contacts !== prevState.contacts) {
+
+      console.log("Updated contacts!!!");
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+
+    };
   };
 
   render() {
